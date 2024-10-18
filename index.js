92d00edf9d4ca29e9e9d9d891881bcbe475acf81
ryanheat <ryan.p.heatley@gmail.com>
2024-10-18 18:15:10 -0500

    added animations

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-function fetchRecipes(){
+function fetchRecipes() {
     return fetch("./data.json")
-    .then(response => response.json())
-    .then(json =>{
-        return json;
-    });
+        .then(response => response.json())
+        .then(json => {
+            return json;
+        });
 }
 
 function loadRecipes(recipes) {
-    const arrayMovies = Object.values(recipes);
-    var CardRecipe = document.getElementById("recipe-list");
-    CardRecipe.innerHTML = "";
+    const arrayRecipes = Object.values(recipes);
+    const cardRecipeContainer = document.getElementById("recipe-list");
+    cardRecipeContainer.innerHTML = "";
 
     let currentRow; // To keep track of the current row
 
-    for (let i = 0; i < arrayMovies.length; i++) {
+    for (let i = 0; i < arrayRecipes.length; i++) {
         // Create a new row for every two recipes
         if (i % 2 === 0) {
             currentRow = document.createElement("div");
             currentRow.classList.add("row", "mb-3"); // Add a margin-bottom for spacing between rows
-            CardRecipe.appendChild(currentRow); // Append the row to the recipe list container
+            cardRecipeContainer.appendChild(currentRow); // Append the row to the recipe list container
         }
 
-        let recipe = arrayMovies[i];
+        let recipe = arrayRecipes[i];
         let id = recipe.id;
         let description = recipe.description;
         let title = recipe.title;
         let image = recipe.image;
 
         let addCardRecipe = document.createElement("div");
-        addCardRecipe.classList.add("col-sm-6"); // Each card takes up half the row (6 columns in a 12-column grid)
+        addCardRecipe.classList.add("col-md-6"); // Each card takes up half the row (6 columns in a 12-column grid)
         addCardRecipe.innerHTML = `
-            <div class="card shadow-sm border-primary mb-3">
-                <img src="${image}" class="card-img-top recipe-image" alt="${description}">
-                <div class="card-body">
-                    <h5 class="card-title">${title}</h5>
-                    <a href="recipe.html?id=${id}" class="btn btn-primary">Make ${title}</a>
-                </div>
+            <div class="recipe-card">
+                <img src="${image}" class="recipe-image" alt="${description}">
+                <h3 class="recipe-title">${title}</h3>
+                <p class="recipe-description">${description}</p>
+                <a href="recipe.html?id=${id}" class="btn btn-primary">Make ${title}</a>
             </div>
         `;
 
@@ -50,10 +49,8 @@ function loadRecipes(recipes) {
     }
 }
 
-
 document.addEventListener('DOMContentLoaded', function() {
     if (window.location.pathname.includes('index.html') || window.location.pathname === '/') {
         fetchRecipes().then(recipes => loadRecipes(recipes));
     }
 });
-
